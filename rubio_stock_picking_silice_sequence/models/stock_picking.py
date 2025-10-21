903120d4faf50538e72f62288ec878cb603d8673
ivanparradogarcia <ivanparradogarcia@gmail.com>
2025-10-21 12:04:18 +0200

    silicie report code clean

--- rubio_stock_picking_silice_sequence/models/stock_picking.py
+++ rubio_stock_picking_silice_sequence/models/stock_picking.py
@@ -10,22 +10,6 @@ class StockPicking(models.Model):
         index=True,
         help="Número de secuencia Silíce asignado automáticamente al validar el albarán de salida",
     )
-    silicie_movement_type = fields.Selection(
-        selection=[
-            ('A08', 'Salida a consumo - territorio interior'),
-            ('A09', 'Salida a consumo - Canarias, Ceuta o Melilla'),
-            ('A10', 'Salida en régimen suspensivo - UE'),
-            ('A11', 'Salida en régimen suspensivo - exportación'),
-            ('A12', 'Salida para entrega exenta'),
-            ('A13', 'Salida para uso de las fuerzas armadas de un Estado miembro'),
-            ('A14', 'Salida para venta a bordo'),
-            ('A15', 'Otras salidas'),
-            ('A16', 'Salida para destrucción'),
-            ('A17', 'Salida a otro depósito fiscal del mismo titular'),
-        ],
-        string='Tipo de Movimiento SILICIE',
-        help='Tipo de movimiento SILICIE para esta entrega.'
-    )
 
     def button_validate(self):
         for picking in self:
